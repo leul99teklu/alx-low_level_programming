ea87eb6d59a070ebe2904ca7d9c2e0fcbf909eb0
leul99teklu <leul99teklu@gmail.com>
2022-04-12 18:23:32 -0400

    _putchar and main header added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void print_alphabet(void);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+main(void)
+{
+int ch;
+ch = 97;
+while (ch < 123)
+{
+_putchar("%c", ch);
+ch++;
+}
+_putchar("\n");
+}
+
