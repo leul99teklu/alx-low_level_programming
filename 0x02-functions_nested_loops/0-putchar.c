fa03ff44c37a32f7d31b7aa1e977c2d6e78ad7eb
leul99teklu <leul99teklu@gmail.com>
2022-04-12 18:59:34 -0400

    _putchar added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - _putchar - writes _putchar to stdout.
+ * main - Prints _putchar as a message.
  *
- * Return: On success 0
+ * Return: Always 0 (Success) 
  */
 int main(void)
 {
