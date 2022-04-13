0bfc17985cb3ea136da28d1175aa13714d9624bd
leul99teklu <leul99teklu@gmail.com>
2022-04-13 18:00:52 -0400

    project debbuging taskes commited from 0-3

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - test input number if it is positive or negative.
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postivive_or_negative(i);
+
+	return (0);
+}
+
