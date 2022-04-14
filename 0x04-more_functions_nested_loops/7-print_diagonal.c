a6a09d3571ff69c9872e292eb188e0be90c27dd8
leul99teklu <leul99teklu@gmail.com>
2022-04-14 19:54:29 -0400

    task 7 print \ for n number of times

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_line - printes diagonal using miltiple \.
+*@n: number of times the character is printed.
+*Return: no return.
+*/
+void print_line(int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		_putchar(92);
+	}
+	_putchar('\n');
+}
