58c13f3f4383c3ed7da42da27770292466db87d9
leul99teklu <leul99teklu@gmail.com>
2022-04-14 18:42:44 -0400

    task 3 print form 0 -9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_numbers - prints from 0 - 9.
+*
+*Return: no return.
+*/
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
