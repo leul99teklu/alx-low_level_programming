f04eaa875d7ad8b40b150f472937763dc1baf83d
leul99teklu <leul99teklu@gmail.com>
2022-04-14 20:04:52 -0400

    task 7 print \ for n number of times

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,7 +11,13 @@ void print_diagonal(int n)
 
 	for (j = 0; j < n; j++)
 	{
+		for (i = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
 		_putchar(92);
+		if (i < (n - 1))
+			_putchar('\n');
 	}
 	_putchar('\n');
 }
