fd6c70d43973bf7b99368de324ef8d4b09fcdb2c
leul99teklu <leul99teklu@gmail.com>
2022-05-09 07:37:27 -0400

    Task 2 print file name using predifined macron

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - print file name using predfind macron followd by new line.
+*
+*Return: 0
+*/
+
+int main(void)
+{
+	printf("File name is: %s\n", __FILE__);
+
+	return (0);
+}
