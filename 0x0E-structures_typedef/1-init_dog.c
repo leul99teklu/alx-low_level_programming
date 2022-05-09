c44e59dea5d4d0c922b326533776f85431279ef3
leul99teklu <leul99teklu@gmail.com>
2022-05-09 09:47:42 -0400

    task 1 bug fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,9 +9,9 @@
  *
  * Return: no return.
  */
- void init_dog(struct dog *d,char *name, float age, char *owner)
- {
- 	if (d)
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
